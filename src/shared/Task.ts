fd6c0c8043ddb9414d62f88b0309b6d48dfc5f95
Paul Leverett <pleverett@gmail.com>
2023-03-02 13:54:45 -0800

    Init commit

---
+++ src/shared/Task.ts
@@ -0,0 +1,16 @@
+import { Entity, Fields } from 'remult'
+
+@Entity("tasks", {
+  allowApiCrud: true
+})
+
+export class Task {
+  @Fields.autoIncrement()
+  id = 0
+
+  @Fields.string()
+  title = ""
+
+  @Fields.boolean()
+  completed = false
+}
